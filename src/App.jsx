d1290dfd748a823279af10d30dbf064d04bdb020
Kean <keanjieden@gmail.com>
2022-03-26 22:15:41 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Pokemon from "./Components/Pokemon/Pokemon";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Pokemon />
+    </React.Fragment>
+  );
+};
+
+export default App;
